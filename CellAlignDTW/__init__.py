d8e630b5086614427b989bf564c5fa12c7ddf79d
Mamie Wang <wangm10@mskcc.org>
2024-11-15 12:36:45 -0500

    Added enrichr function

--- CellAlignDTW/__init__.py
+++ CellAlignDTW/__init__.py
@@ -1,3 +1,3 @@
 from ._align import CellAlignDTW
 from ._gam import gam_smooth_expression
-from ._cluster import process_gene_data
+from ._cluster import process_gene_data, enrichr
--- CellAlignDTW/_cluster.py
+++ CellAlignDTW/_cluster.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from scipy.cluster.hierarchy import linkage, leaves_list
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
 from tslearn.clustering import KShape, KernelKMeans
 from collections import defaultdict
+import gseapy as gp
+import time
 
 def order_categories_by_average_position(categories):
     # Dictionary to store total positions and counts
@@ -47,9 +51,9 @@ def categorize_values(X, Y):
 
 
 def process_gene_data(scores_df, gene_curve, cell_colors, cutoff_points,
-                      MI_threshold=0.5, n_clusters=3,
-                      label_orders=None, n_init=5):
-    is_filtered_genes = scores_df.gene[(scores_df.MI > MI_threshold)]
+                      MI_threshold=0, GCV_threshold=1, AIC_threshold=np.Inf, n_clusters=3,
+                      label_names=None, n_init=5, hierarchical=False):
+    is_filtered_genes = scores_df.gene[(scores_df.MI > MI_threshold) & (scores_df.GCV < GCV_threshold) & (scores_df.AIC < AIC_threshold)]
     aligned_score = gene_curve.aligned_score
     gene_curve = gene_curve.loc[:, is_filtered_genes]
     zscore_scaler = StandardScaler()
@@ -59,24 +63,45 @@ def process_gene_data(scores_df, gene_curve, cell_colors, cutoff_points,
         columns=gene_curve.columns,
         index=gene_curve.index
     )
-    ks = KShape(n_clusters=n_clusters, random_state=0, 
-                verbose=True, n_init=n_init).fit(normalized_gene_curve.values.T)
-    clusters = ks.labels_
+    if hierarchical:
+        hc = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward')
+        clusters = hc.fit_predict(normalized_gene_curve.values.T)
+    else:
+        ks = KShape(n_clusters=n_clusters, random_state=0, 
+                    verbose=True, n_init=n_init).fit(normalized_gene_curve.values.T)
+        clusters = ks.labels_
 
-    max_index = normalized_gene_curve.apply(lambda col: max_idx(col, 3), axis=0)
+    max_index = normalized_gene_curve.apply(lambda col: max_idx(col, 400), axis=0)
     combined_order = sorted(range(len(max_index)), key=lambda i: (max_index[i]))
     sorted_gene_curve = normalized_gene_curve.iloc[:, combined_order].T
-
     labels = clusters[combined_order]
-    label_ordering = order_categories_by_average_position(labels)
-    if label_orders is None: 
-        label_orders = range(n_clusters)
-    map_gene_labels = dict(zip(range(n_clusters), label_orders))
-    category = [map_gene_labels[x] for x in labels]
-    lut = dict(zip(label_orders, sns.hls_palette(len(label_orders), l=0.5, s=0.8)))
-    row_colors = pd.DataFrame(category)[0].map(lut)
+    label_orders = order_categories_by_average_position(labels)
+    new_label_mapping = {old_label: new_label for new_label, old_label in enumerate(label_orders)}
+    labels = pd.Series(labels).map(new_label_mapping)
 
-    lut = dict(zip(range(len(cell_colors)), cell_colors), l=0.5, s=0.8)
-    col_cols = (categorize_values(aligned_score, cutoff_points)).map(lut)
+    if label_names is None: 
+        category = labels
+    else:
+        map_gene_labels = dict(zip(range(n_clusters), label_names))
+        category = [map_gene_labels[x] for x in labels]
+    row_colors = pd.DataFrame(category)[0]
+    col_cols = (categorize_values(aligned_score, cutoff_points))
 
     return sorted_gene_curve, row_colors, col_cols, category
+
+def enrichr(gene_list, gene_sets):
+    while True:
+        try:
+            enr = gp.enrichr(
+                gene_list=gene_list,
+                gene_sets=gene_sets,
+                organism='human',
+                outdir=None,
+                cutoff=1,
+                
+            )
+            early_results = enr.results
+            return early_results  # Return the results if successful
+        except Exception as e:
+            print(f"An error occurred: {e}. Retrying...")
+            time.sleep(5)
