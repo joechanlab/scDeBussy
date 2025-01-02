324c1accf02aa5d15c543479c13136a47612f830
Mamie Wang <wangm10@mskcc.org>
2025-01-02 11:25:39 -0500

    Moved GMM cutoff points determination function to utils

--- CellAlignDTW/_align.py
+++ CellAlignDTW/_align.py
@@ -7,7 +7,7 @@ from scipy.optimize import brentq
 from tslearn.metrics import dtw_path
 from scipy.optimize import curve_fit
 from ._dba import dtw_barycenter_averaging_with_categories
-from ._utils import split_by_cutpoints
+from ._utils import split_by_cutpoints, compute_gmm_cutpoints
 
 class CellAlignDTW:
     def __init__(self, df, cluster_ordering, subject_col, score_col, cell_id_col, cell_type_col, verbose=False):
@@ -23,11 +23,11 @@ class CellAlignDTW:
         self.verbose = verbose
 
     def align(self):
-        self.compute_cutoff_points_kmeans()
+        self.compute_cutoff_points_gmm()
         aligned_segments = self.align_with_continuous_barycenter()
         self.create_aligned_dataframe(aligned_segments)
 
-    def compute_cutoff_points_kmeans(self):
+    def compute_cutoff_points_gmm(self):
         cutoff_points = {}
         subjects = self.df[self.subject_col].unique()
         num_clusters = len(self.cluster_ordering)
@@ -38,32 +38,7 @@ class CellAlignDTW:
                 subject_data[self.score_col].values,
                 subject_data['numeric_label'].values
             ])
-            
-            # Fit GMM instead of KMeans
-            gmm = GaussianMixture(n_components=num_clusters, random_state=42)
-            gmm.fit(X)
-            
-            # Sort components by their means
-            means = gmm.means_[:, 0]  # Get means of the score dimension
-            sorted_indices = np.argsort(means)
-            
-            # Calculate intersection points between adjacent Gaussians
-            cutoffs = []
-            for i in range(len(sorted_indices)-1):
-                idx1, idx2 = sorted_indices[i], sorted_indices[i+1]
-                mu1, sigma1 = means[idx1], np.sqrt(gmm.covariances_[idx1][0,0])
-                mu2, sigma2 = means[idx2], np.sqrt(gmm.covariances_[idx2][0,0])
-                
-                # Find intersection point of the two Gaussians
-                def gaussian_diff(x):
-                    return (stats.norm.pdf(x, mu1, sigma1) * gmm.weights_[idx1] - 
-                        stats.norm.pdf(x, mu2, sigma2) * gmm.weights_[idx2])
-                
-                # Search for zero crossing between the means
-                cutoff = brentq(gaussian_diff, mu1, mu2)
-                cutoffs.append(cutoff)
-            
-            cutoff_points[subject] = cutoffs
+            cutoff_points[subject] = compute_gmm_cutpoints(X, num_clusters)
         
         self.cutoff_points = cutoff_points
         print(cutoff_points)
@@ -78,18 +53,11 @@ class CellAlignDTW:
         barycenter_categories = [self.label_mapping[x] for x in barycenter_categories]
         
         X = np.column_stack([
-        continuous_barycenter,
-        barycenter_categories
+            continuous_barycenter,
+            barycenter_categories
         ])
         
-        kmeans = KMeans(n_clusters=len(self.cluster_ordering), random_state=42)
-        kmeans.fit(X)
-        
-        # Sort centers by x-coordinate and calculate midpoints
-        centers = sorted(kmeans.cluster_centers_, key=lambda x: x[0])
-        reference_cutpoints = [(centers[i][0] + centers[i+1][0])/2 for i in range(len(centers)-1)]
-        
-        print("Reference cutpoints:", reference_cutpoints)
+        reference_cutpoints = compute_gmm_cutpoints(X, len(self.cluster_ordering))
 
         barycenter_segments = split_by_cutpoints(pd.DataFrame({self.score_col: continuous_barycenter}), reference_cutpoints, self.score_col)
 
--- CellAlignDTW/_utils.py
+++ CellAlignDTW/_utils.py
@@ -1,6 +1,22 @@
+import numpy as np
 import pandas as pd
+from sklearn.mixture import GaussianMixture
+import scipy.stats as stats
+from scipy.optimize import brentq
+
 
 def split_by_cutpoints(df, cutpoints, score_col):
+    """
+    Split a DataFrame into segments based on cutpoints in a specified column.
+
+    Args:
+        df (pd.DataFrame): DataFrame to be split.
+        cutpoints (list): List of cutpoints to split the DataFrame.
+        score_col (str): Column name in the DataFrame to split based on.
+
+    Returns:
+        list: List of DataFrames, each representing a segment split by the cutpoints.
+    """
     segments = [[] for _ in range(len(cutpoints) + 1)]
     
     for _, row in df.iterrows():
@@ -14,3 +30,43 @@ def split_by_cutpoints(df, cutpoints, score_col):
     
     segments = [pd.DataFrame(segment) for segment in segments]
     return segments
+
+
+def compute_gmm_cutpoints(X, n_components):
+    """
+    Compute cutoff points between clusters using Gaussian Mixture Models.
+    
+    Args:
+        X: array-like of shape (n_samples, 2)
+           First column contains scores, second column contains numeric labels
+        n_components: int
+           Number of components/clusters to fit
+           
+    Returns:
+        list: Sorted cutoff points between adjacent Gaussian components
+    """
+    # Fit GMM
+    gmm = GaussianMixture(n_components=n_components, random_state=42)
+    gmm.fit(X)
+    
+    # Sort components by their means
+    means = gmm.means_[:, 0]  # Get means of the score dimension
+    sorted_indices = np.argsort(means)
+    
+    # Calculate intersection points between adjacent Gaussians
+    cutoffs = []
+    for i in range(len(sorted_indices)-1):
+        idx1, idx2 = sorted_indices[i], sorted_indices[i+1]
+        mu1, sigma1 = means[idx1], np.sqrt(gmm.covariances_[idx1][0,0])
+        mu2, sigma2 = means[idx2], np.sqrt(gmm.covariances_[idx2][0,0])
+        
+        # Find intersection point of the two Gaussians
+        def gaussian_diff(x):
+            return (stats.norm.pdf(x, mu1, sigma1) * gmm.weights_[idx1] - 
+                   stats.norm.pdf(x, mu2, sigma2) * gmm.weights_[idx2])
+        
+        # Search for zero crossing between the means
+        cutoff = brentq(gaussian_diff, mu1, mu2)
+        cutoffs.append(cutoff)
+    
+    return cutoffs
