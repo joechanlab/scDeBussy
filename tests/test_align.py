b0ec5287707c9d0b745cf31f9a4c5fc51dfafd65
Mamie Wang <wangm10@mskcc.org>
2025-03-17 11:25:25 -0400

    Added github workflow to test alignment

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,8 +1,8 @@
 import numpy as np
-from scDeBussy import scDeBussy
+from scDeBussy import aligner
 
 def test_scDeBussy_initialization(sample_df, cluster_ordering):
-    aligner = scDeBussy(
+    test_aligner = aligner(
         df=sample_df,
         cluster_ordering=cluster_ordering,
         subject_col='subject',
@@ -11,12 +11,12 @@ def test_scDeBussy_initialization(sample_df, cluster_ordering):
         cell_type_col='cell_type'
     )
     
-    assert aligner.df.equals(sample_df)
-    assert aligner.cluster_ordering == cluster_ordering
-    assert aligner.cutoff_points is None
+    assert test_aligner.df.equals(sample_df)
+    assert test_aligner.cluster_ordering == cluster_ordering
+    assert test_aligner.cutoff_points is None
 
 def test_scDeBussy_align(sample_df, cluster_ordering):
-    aligner = scDeBussy(
+    test_aligner = aligner(
         df=sample_df,
         cluster_ordering=cluster_ordering,
         subject_col='subject',
@@ -25,7 +25,7 @@ def test_scDeBussy_align(sample_df, cluster_ordering):
         cell_type_col='cell_type'
     )
     
-    aligner.align()
-    assert aligner.cutoff_points is not None
-    assert np.isin('aligned_score', aligner.df.columns).any()
+    test_aligner.align()
+    assert test_aligner.cutoff_points is not None
+    assert np.isin('aligned_score', test_aligner.df.columns).any()
 
